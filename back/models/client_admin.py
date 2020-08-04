891312fff76ea6534352a485bfbaaa9b1f14dd92
Nikolay <nchernov@gmail.com>
2020-08-04 14:26:13 +0300

    fix file names

--- back/models/client_admin.py
+++ back/models/client_admin.py
@@ -1,5 +1,12 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from . import FoxUser
 
 
-class ClientAdmin(models.Model):
-    pass
+class ClientAdmin(FoxUser):
+    class Role(models.TextChoices):
+        admin = "Admin", _("Administrator")
+
+    role = models.CharField(
+        max_length=10, choices=Role.choices, default=Role.admin,
+    )
