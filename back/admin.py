10d53d6c03b5aa4c144ecb96d4b7cdabdec14c84
Taraslysak <lysak.ipr@gmail.com>
2020-08-12 19:33:38 +0300

    Added company to admin

--- back/admin.py
+++ back/admin.py
@@ -10,7 +10,7 @@ from .forms import (
     ClientManagerCreationForm,
     ContractorCreationForm,
 )
-from .models import ClientAdmin, ClientManager, Contractor
+from .models import ClientAdmin, ClientManager, Contractor, Company
 
 FoxUser = get_user_model()
 
@@ -79,7 +79,60 @@ class ContractorAdmin(FoxUserAdmin):
     add_form = ContractorCreationForm
 
 
+class ClientAdminInline(admin.StackedInline):
+    extra = 1
+    model = ClientAdmin
+    form = ClientAdminCreationForm
+    fieldsets = (
+        (
+            None,
+            {
+                "description": (
+                    "Enter the new user's name and email address and click save."
+                    " The user will be emailed a link allowing them to login to"
+                    " the site and set their password."
+                ),
+                "fields": ("email", "username",),
+            },
+        ),
+        (
+            "Password",
+            {
+                "description": "Optionally, you may set the user's password here.",
+                "fields": ("password1", "password2", "role"),
+                "classes": ("collapse", "collapse-closed"),
+            },
+        ),
+    )
+
+    def save_model(self, request, obj, form, change):
+        if not change and not obj.has_usable_password():
+            # Django's PasswordResetForm won't let us reset an unusable
+            # password. We set it above super() so we don't have to save twice.
+            obj.set_password(get_random_string())
+            reset_password = True
+        else:
+            reset_password = False
+
+        super(FoxUserAdmin, self).save_model(request, obj, form, change)
+
+        if reset_password:
+            reset_form = PasswordResetForm({"email": obj.email})
+            assert reset_form.is_valid()
+            reset_form.save(
+                subject_template_name="registration/account_creation_subject.txt",
+                email_template_name="registration/account_creation_email.html",
+            )
+
+
+class CompanyAdmin(admin.ModelAdmin):
+    inlines = [
+        ClientAdminInline,
+    ]
+
+
 admin.site.register(get_user_model(), FoxUserAdmin)
 admin.site.register(ClientAdmin, ClientAdminAdmin)
 admin.site.register(ClientManager, ClientManagerAdmin)
 admin.site.register(Contractor, ContractorAdmin)
+admin.site.register(Company, CompanyAdmin)
