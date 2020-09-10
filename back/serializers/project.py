54339906983fe8b757eff624e6c87485a3e822d5
Taraslysak <lysak.ipr@gmail.com>
2020-09-10 08:54:03 +0300

    project approval/work status backend

--- back/serializers/project.py
+++ back/serializers/project.py
@@ -24,13 +24,13 @@ class ProjectListSerializer(serializers.ModelSerializer):
         status = obj.status
         status_options = {
             "Created": "Created",
-            "Submitted": "Submitted ",
-            "Approved ": "Approved ",
+            "Submitted": "Submitted",
+            "Approved": "Approved",
             "Rejected": "Rejected",
             "Closed": "Closed",
         }
         application_status = (
-            status_options[status] if status in status_options else "Approved"
+            status_options[status] if status in status_options.keys() else "Approved"
         )
         return application_status
 
@@ -38,14 +38,14 @@ class ProjectListSerializer(serializers.ModelSerializer):
         status = obj.status
         status_options = {
             "Approved": "Ready to start",
-            "Works_started ": "Works started ",
-            "Works_finished ": "Works finished",
+            "Works_started": "Works started",
+            "Works_finished": "Works finished",
             "Extended": "Extended",
             "Closed": "Closed",
         }
         work_status = (
             status_options[status]
-            if status in status_options
+            if status in status_options.keys()
             else "Application processing"
         )
         return work_status
