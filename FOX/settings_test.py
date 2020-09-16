9a529dc9822e9ef6f91d5e6f6aa16b06a83458ad
Nikolay <nchernov@gmail.com>
2020-09-16 12:15:34 +0300

    use SQLite for tesing

---
+++ FOX/settings_test.py
@@ -0,0 +1,10 @@
+from FOX.settings import *
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'test_database',
+    }
+}
